8ff8d15d1288ac5efc1a052b2ae28a60d6cec416
Thomas ROBERT <thomas_3004@hotmail.fr>
2025-07-26 09:40:11 +0200

    Refactor components and navbar structure; remove unused files and improve styling
    - Updated About, Footer, ProjectCard, Projects, Skills, and SkillItem components for consistency.
    - Deleted NavLink and Navbar components; replaced with a new navbar structure.
    - Introduced MobileMenu for responsive navigation.
    - Created links.ts for centralized navigation and social link management.
    - Enhanced global CSS with new design tokens and improved dark mode support.
    - Updated 404 and index pages to reflect new component structure.

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,12 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  endOfLine: 'lf',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
   overrides: [
     {
       files: '*.astro',
---
+++ src/components/navbar/links.ts
@@ -0,0 +1,17 @@
+export const navlinks = [
+  { href: '#projects', text: 'Mes projets' },
+  { href: '#skills', text: 'Mes compétences' },
+];
+
+export const socialLinks = [
+  {
+    url: 'https://github.com/Fixito',
+    icon: { pack: 'ri', name: 'github-fill' },
+    title: 'GitHub',
+  },
+  {
+    url: 'https://www.linkedin.com/in/thomasrobert1/',
+    icon: { pack: 'ri', name: 'linkedin-box-fill' },
+    title: 'Linkedin',
+  },
+];
