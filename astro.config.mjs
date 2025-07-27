77c59d73bcdacc05bc4bcc3877ce8e9e9c791462
Thomas ROBERT <thomas_3004@hotmail.fr>
2025-07-27 16:27:27 +0200

    feat: add sitemap integration and improve SEO metadata across pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thomasrobert.netlify.app',
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
